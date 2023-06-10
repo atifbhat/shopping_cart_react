d4f60fd0ec186980e08b9e65d9325b53fd54bf23
atifsalim <atifsalimbhat@gmail.com>
2023-06-10 16:14:47 +0530

    checkout done

--- src/App.js
+++ src/App.js
@@ -1,105 +1,117 @@
-import React, { useState } from "react";
-import data from "./data.json";
-import Filter from "./components/Filter";
-import Cart from "./components/Cart";
-import Products from "./components/Products";
-
+import React, { useState, useEffect } from 'react';
+import data from './data.json';
+import Filter from './components/Filter';
+import Cart from './components/Cart';
+import Products from './components/Products';
 
 const App = () => {
+  const [products, setProducts] = useState(data.products);
+
+  const cartItemsFromStorage = localStorage.getItem('cartItems');
+  const initialCartItems = cartItemsFromStorage
+    ? JSON.parse(cartItemsFromStorage)
+    : [];
+  const [cartItems, setCartItems] = useState(initialCartItems);
+
+  const [size, setSize] = useState('');
+  const [sort, setSort] = useState('');
+    
+  
+ const createOrder=(order)=>{
+    alert("Need to save order for" + order.name);
+  };
  
-const [products, setProducts] = useState(data.products);
-const [cartItems, setCartItems] = useState([]);
-const [size, setSize] = useState("");
-const [sort, setSort] = useState("");
+  const removeFromCart = (product) => {
+    const updatedCartItems = cartItems.filter(
+      (item) => item._id !== product._id
+    );
+    setCartItems(updatedCartItems);
+  };
 
-const removeFromCart =(product) =>{
-
-const updatedCartItems= [...cartItems];
-const updatedCart=updatedCartItems.filter((item) => item._id !== product._id);
-  setCartItems(updatedCart);
-};
-
-const addToCart = (product) => {
-  const updatedCartItems = [...cartItems];
-  let alreadyInCart = false;
-  updatedCartItems.forEach((item) => {
-    if (item._id === product._id) {
-      item.count++;
-      alreadyInCart = true;
+  const addToCart = (product) => {
+    const existingCartItem = cartItems.find((item) => item._id === product._id);
+    if (existingCartItem) {
+      setCartItems(
+        cartItems.map((item) =>
+          item._id === product._id ? { ...item, count: item.count + 1 } : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, count: 1 }]);
     }
-  });
+  };
 
-  if (!alreadyInCart) {
-    updatedCartItems.push({ ...product, count: 1 });
-  }
+  const sortProducts = (event) => {
+    const sortValue = event.target.value;
+    setSort(sortValue);
 
-  setCartItems(updatedCartItems);
-  localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
+    setProducts((prevProducts) => {
+      const sortedProducts = [...prevProducts];
 
-};
+      sortedProducts.sort((a, b) => {
+        if (sortValue === 'lowest') {
+          return a.price - b.price;
+        } else if (sortValue === 'highest') {
+          return b.price - a.price;
+        } else {
+          return a._id - b._id;
+        }
+      });
 
-const sortProducts = (event) => {
-  const sortValue = event.target.value;
-  setSort(sortValue);
+      return sortedProducts;
+    });
+  };
 
-  setProducts((prevProducts) =>
-    [...prevProducts].sort((a, b) => {
-      if (sortValue === "lowest") {
-        return a.price > b.price ? 1 : -1;
-      } else if (sortValue === "highest") {
-        return a.price < b.price ? 1 : -1;
-      } else {
-        return a._id < b._id ? 1 : -1;
-      }
-    })
+  const filterProducts = (event) => {
+    const selectedSize = event.target.value;
+
+    setSize(selectedSize);
+
+    if (selectedSize === '') {
+      setProducts(data.products);
+    } else {
+      setProducts(
+        data.products.filter(
+          (product) => product.availableSizes.indexOf(selectedSize) >= 0
+        )
+      );
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React shopping cart</a>
+      </header>
+
+      <main>
+        <div className="content">
+          <div className="main">
+            <Filter
+              count={products.length}
+              size={size}
+              sort={sort}
+              filterProducts={filterProducts}
+              sortProducts={sortProducts}
+            />
+            <Products products={products} addToCart={addToCart} />
+          </div>
+          <div className="sidebar">
+            <Cart 
+            cartItems={cartItems}
+             removeFromCart={removeFromCart}
+             createOrder={createOrder} />
+          </div>
+        </div>
+      </main>
+
+      <footer>All rights reserved.</footer>
+    </div>
   );
 };
 
-const filterProducts = (event) => {
-  const selectedSize = event.target.value;
-
-  if (selectedSize === "") {
-    setSize(selectedSize);
-    setProducts(data.products);
-  } else {
-    setSize(selectedSize);
-    setProducts(data.products.filter((product) => product.availableSizes.indexOf(selectedSize) >= 0));
-  }
-};
-
-
-return (
-  <div className="grid-container">
-    <header>
-      <a href="/">React shopping cart</a>
-    </header>
-
-    <main>
-      <div className="content">
-        <div className="main">
-          <Filter
-            count={products.length}
-            size={size}
-            sort={sort}
-            filterProducts={filterProducts}
-            sortProducts={sortProducts}
-          ></Filter>
-          <Products products={products} addToCart={addToCart}></Products>
-        </div>
-        <div className="sidebar">
-          <Cart 
-          cartItems={cartItems}
-          removeFromCart={removeFromCart}
-          />
-        </div>
-      </div>
-    </main>
-
-    <footer>All rights reserved.</footer>
-  </div>
-);
-
-
-}
 export default App;
-
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,67 +1,139 @@
-import React from 'react';
+import React, { useState } from 'react';
 import formatCurrency from '../util';
 
 const Cart = (props) => {
-  
-    const { cartItems } = props;
-  
+  console.log(props);
+  const [checkout, setCheckout] = useState(false);
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const { cartItems, removeFromCart } = props;
 
-    return (
-      <div>
-      
-        {cartItems.length === 0 ? (
-          <div className="cart cart-header">Cart is empty</div>
-        ) : (
-          <div className="cart cart-header">
-            You have {cartItems.length} item(s) in the cart
+  if (!Array.isArray(cartItems)) {
+    return <div>Loading cart items...</div>;
+  }
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'name') {
+      setName(value);
+    } else if (name === 'address') {
+      setAddress(value);
+    }
+  };
+
+  const submitOrder = (e) => {
+    e.preventDefault();
+    const order = {
+      name,
+      email,
+      address,
+      cartItems,
+    };
+    // TODO: Handle submitting the order data
+    console.log(order);
+    props.createOrder(order);
+    
+    
+  };
+
+  return (
+    <div>
+      {cartItems.length === 0 ? (
+        <div className="cart cart-header">Cart is empty</div>
+      ) : (
+        <div className="cart cart-header">
+          You have {cartItems.length} item(s) in the cart
+        </div>
+      )}
+
+      <div className="cart">
+        <ul className="cart-items">
+          {cartItems.map((item) => (
+            <li key={item._id}>
+              <div>
+                <img src={item.image} alt={item.title}></img>
+              </div>
+
+              <div>{item.title}</div>
+
+              <div className="right">
+                {formatCurrency(item.price)} * {item.count}{' '}
+                <button className="button" onClick={() => removeFromCart(item)}>
+                  Remove
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {cartItems.length !== 0 && (
+        <div>
+          <div className="cart">
+            <div className="total">
+              <div>
+                Total:{' '}
+                {formatCurrency(
+                  cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                )}
+              </div>
+              <button
+                className="button primary"
+                onClick={() => setCheckout(true)}
+              >
+                Proceed
+              </button>
+            </div>
           </div>
-        )}
 
-<div className='cart'>
-  <ul className='cart-items'>
-    {cartItems.map(item=>(
-      
-      <li key={item._id}>
-        <div>
-          <img src={item.image} alt={item.title}></img>
+          {checkout && (
+            <div className="cart">
+              <form onSubmit={submitOrder}>
+                <ul className="form-container">
+                  <li>
+                    <label>Email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      required
+                      onChange={handleInput}
+                    />
+                  </li>
+                  <li>
+                    <label>Name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      required
+                      onChange={handleInput}
+                    />
+                  </li>
+                  <li>
+                    <label>Address</label>
+                    <input
+                      type="text"
+                      name="address"
+                      required
+                      onChange={handleInput}
+                    />
+                  </li>
+                  <li>
+                    <button  className="button primary" type="submit">
+                      Place Order
+                    </button>
+                  </li>
+                </ul>
+              </form>
+            </div>
+          )}
         </div>
-
-        <div>
-          {item.title}
-        </div>
-
-        <div className='right'>
-          {formatCurrency(item.price)}* {item.count} {" "}
-        
-<button className='button'
- onClick={()=>props.removeFromCart(item)}>
-  Remove
-  </button>
-  </div>
-      </li>
-    ))}
-  </ul>
-</div>
-
-{cartItems.length !==0 && (
-  <div className='cart'>
-    <div className='total'>
-      <div> 
-        Total :{" "}
-        {formatCurrency(
-          cartItems.reduce((a,c)=> a+ c.price * c.count, 0)
-        )}
-      </div>
-      <button className='button primary'>Proceed</button>
-
+      )}
     </div>
-    </div>
-)}
+  );
+};
 
-      </div>
-
-
-    );
-        }
-      
 export default Cart;
